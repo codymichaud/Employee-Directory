a88e892d3bce3786abd5ac850017285f488de07c
Cody Michaud <cody.r.michaud@gmail.com>
2021-05-09 10:38:09 -0700

    Fixed search error

--- employee-directory/src/Components/EmploySearch/index.js
+++ employee-directory/src/Components/EmploySearch/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BsSearch } from 'react-icons/bs';
 import './employSearch.css';
 
 // Function to allow searching for a specific user
@@ -9,9 +8,7 @@ function EmploySearch(props) {
         <form>
             <div className='firstForm'>
                 <div className='inputField'>
-                    <div className='icon'>
-                        <BsSearch />
-                    </div>
+
                     <input
                         value={props.search}
                         onChange={props.handleInputChange}
